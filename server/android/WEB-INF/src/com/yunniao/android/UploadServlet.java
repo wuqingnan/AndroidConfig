124887aa1e71279ebe3b79f90605906556f45a68
史忠勇 <shizhongyong@yunniao.me>
2018-05-28 18:19:17 +0800

    增加N端

--- server/android/WEB-INF/src/com/yunniao/android/UploadServlet.java
+++ server/android/WEB-INF/src/com/yunniao/android/UploadServlet.java
@@ -30,8 +30,8 @@ public class UploadServlet extends HttpServlet {
 
 	// 上传配置
 	private static final int MEMORY_THRESHOLD = 1024 * 1024 * 3; // 3MB
-	private static final int MAX_FILE_SIZE = 1024 * 1024 * 40; // 40MB
-	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 50; // 50MB
+	private static final int MAX_FILE_SIZE = 1024 * 1024 * 400; // 40MB
+	private static final int MAX_REQUEST_SIZE = 1024 * 1024 * 450; // 50MB
 
 	/**
 	 * @see HttpServlet#HttpServlet()
